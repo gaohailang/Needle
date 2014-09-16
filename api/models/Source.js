32a7bb4678ca0984ae2018e1b26fd81f646d8686
Wangye Zhao | 赵望野 <wangye.zhao@wandoujia.com>
2014-09-16 18:23:55 +0800

    Use S3 as file storage.

--- api/models/Source.js
+++ api/models/Source.js
@@ -1,15 +1,54 @@
-/**
-* Source.js
-*
-* @description :: TODO: You might write a short summary of how this model works and what it represents here.
-* @docs        :: http://sailsjs.org/#!documentation/models
-*/
+var AWS = require('aws-sdk');
+
+AWS.config.loadFromPath('./OathKeeper/frontend/aws.json');
+
+var s3 = new AWS.S3();
+var zlib = require('zlib');
 
 module.exports = {
-
     attributes: {
         name: 'string',
         body: 'string'
+    },
+    afterCreate: function (source, cb) {
+        var params = {
+            Bucket: 'web-statics-production',
+            Key: 'needle/' + source.id + '.json',
+            Body: source.body,
+            CacheControl: 'no-cache',
+            ContentType: 'application/json',
+            ACL: 'public-read'
+        };
+
+        s3.putObject(params, function(err, data) {
+            cb();
+        });
+    },
+    afterUpdate: function (source, cb) {
+        var params = {
+            Bucket: 'web-statics-production',
+            Key: 'needle/' + source.id + '.json',
+            Body: source.body,
+            CacheControl: 'no-cache',
+            ContentType: 'application/json',
+            ACL: 'public-read'
+        };
+
+        s3.putObject(params, function(err, data) {
+            cb();
+        });
+    },
+    afterDestroy: function (source, cb) {
+        var params = {
+            Bucket: 'web-statics-production',
+            Key: 'needle/' + source[0].id + '.json'
+        };
+
+        s3.deleteObject(params, function(err, data) {
+            console.log(err);
+            console.log(data);
+            cb();
+        });
     }
 };
 
