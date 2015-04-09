ba94c96c6170e07845cb4985923c1942d68e1319
gaohailang <ghlndsl@126.com>
2015-04-09 10:24:27 +0800

    feat: give breath for getBody/getJSON action by configing policy true and cater to editorconfig~

--- api/controllers/LoginController.js
+++ api/controllers/LoginController.js
@@ -9,21 +9,24 @@ var passport = require('passport');
 
 module.exports = {
 
-  index: function(req, res) {
-    res.view();
-  },
+    index: function(req, res) {
+        res.view();
+    },
 
-  // https://developers.google.com/
-  // https://developers.google.com/accounts/docs/OAuth2Login#scope-param
-  google: function(req, res) {
-    passport.authenticate('google', { failureRedirect: '/login/google', scope: ['https://www.googleapis.com/auth/plus.profile.emails.read'] }, function(err, user) {
-      if(user) {
-        req.session.isLogin = true;
-        res.redirect('/needle');
-      } else {
-        res.view('500');
-        return;
-      }
-    })(req, res);
-  }
+    // https://developers.google.com/
+    // https://developers.google.com/accounts/docs/OAuth2Login#scope-param
+    google: function(req, res) {
+        passport.authenticate('google', {
+            failureRedirect: '/login/google',
+            scope: ['https://www.googleapis.com/auth/plus.profile.emails.read']
+        }, function(err, user) {
+            if (user) {
+                req.session.isLogin = true;
+                res.redirect('/needle');
+            } else {
+                res.view('500');
+                return;
+            }
+        })(req, res);
+    }
 };
